af01e3effc3ca372ad9e38aa03b5158c622e69d3
LuckyAnggara <anggara.lucky1992@gmail.com>
2024-03-27 14:03:02 +0700

    ok

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 
 export const axiosIns = axios.create({
   // baseURL: 'http://127.0.0.1:8000',
-  // baseURL: 'https://de.bbmakmur.com',
+  baseURL: 'https://de.bbmakmur.com',
   // baseURL: 'http://192.168.10.127:8000',
-  baseURL: 'http://192.168.16.128:8000',
+  // baseURL: 'http://192.168.16.128:8000',
   // baseURL: 'http://192.168.46.163:8000',
   withCredentials: true,
   // withXSRFToken: true,
@@ -14,7 +14,8 @@ export const axiosIns = axios.create({
 })
 
 export const authClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://de.bbmakmur.com',
+  // baseURL: 'http://127.0.0.1:8000',
   // baseURL: 'http://192.168.16.128:8000',
   withCredentials: true, // required to handle the CSRF token
 })
