3261ab28e222c9dc95b3b37d212144c54bd046d2
LuckyAnggara <anggara.lucky1992@gmail.com>
2024-12-16 13:42:12 +0700

    test

--- src/views/user/keuangan/perjadin/dialog/component/realisasi/Umum.vue
+++ src/views/user/keuangan/perjadin/dialog/component/realisasi/Umum.vue
@@ -134,7 +134,7 @@
         </div>
         <div>
           <button
-            @click="toSptjm()"
+            @click="downloadSPTJM()"
             type="submit"
             class="w-32 flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-xs font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
           >
@@ -205,8 +205,11 @@ async function toRill() {
   window.open(resolvedRoute.href, '_blank')
 }
 
+async function downloadSPTJM() {
+  perjadinDetailStore.downloadSptjm()
+}
+
 async function toSptjm() {
-  // perjadinDetailStore.downloadSptjm()
   let resolvedRoute = router.resolve({
     name: 'perjadin-ptj-sptjm',
     params: { id: perjadinDetailStore.singleResponse.id },
